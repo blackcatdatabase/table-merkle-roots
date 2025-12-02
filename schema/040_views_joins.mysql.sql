a5460d5dc9658184dda4a0ed6f0d75ee6ed7eebc
Vito416 <vito416@protonmail.com>
2025-12-02 19:27:14 +0100

    Sync submodule state after YAML views + upsert/coverage fixes

--- schema/040_views_joins.mysql.sql
+++ schema/040_views_joins.mysql.sql
@@ -1,7 +1,7 @@
--- Auto-generated from joins-mysql.psd1 (map@mtime:2025-11-27T17:49:37Z)
+-- Auto-generated from joins-mysql.yaml (map@94ebe6c)
 -- engine: mysql
--- view:   merkle_roots_latest
--- Latest Merkle roots per table
+-- view:   merkle_latest
+
 CREATE OR REPLACE ALGORITHM=TEMPTABLE SQL SECURITY INVOKER VIEW vw_merkle_latest AS
 WITH ranked AS (
   SELECT
